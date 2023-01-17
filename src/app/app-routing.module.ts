281797c71d9bdcddbbc67cc2b064fbbff0f37c61
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2023-01-17 09:35:29 -0600

    feat: added router with pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent,
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export function httpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
@@ -16,7 +20,11 @@ export function httpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
